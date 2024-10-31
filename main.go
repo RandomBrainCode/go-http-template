dd1beea3dc51d5e4fc436671a038db241332d54d
Andrew Velasco <andrew@randombraincode.com>
2024-10-30 19:56:54 -0600

    Add JSON response handler to /hello endpoint
    Introduced a new handler function, `helloHandler`, that returns a JSON encoded response. Integrated this handler into the HTTP server with the endpoint `/hello`.

--- main.go
+++ main.go
@@ -1,14 +1,28 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
+type Response struct {
+	Message string `json:"message"`
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	response := Response{Message: "Hello from Go!"}
+	err := json.NewEncoder(w).Encode(response)
+	if err != nil {
+		return
+	}
+}
+
 func main() {
 	server := http.FileServer(http.Dir("./frontend/build"))
 	http.Handle("/", server)
+	http.HandleFunc("/hello", helloHandler)
 
 	fmt.Println("Listening on :8080...\nCtrl+C to exit")
 
